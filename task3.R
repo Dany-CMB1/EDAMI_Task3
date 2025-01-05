ac6de4ed332031ecc5b597b0f1b2a8c8df284e49
Daniel Bannister <daniel.bannister@proton.me>
2025-01-05 21:54:41 +0100

    add: Kmeans, PAM and DBSCAN for CTG

--- task3.R
+++ task3.R
@@ -1,22 +1,27 @@
-
 #Lab3 - Clustering
 
 #authors:
 # --- Daniel BANNISTER
 # --- Achille NOTRE DAME
 
-#data set for the laboratory task
-#http://archive.ics.uci.edu/ml/datasets/Cardiotocography
+library(dbscan)
+library(fpc)
+library(cluster)
+library(factoextra)
 
-download.file('https://staff.elka.pw.edu.pl/~rbembeni/dane/cardioto_noClass_corr.csv','cardioto_noClass_corr.csv')
-ctg_noClass <- read.csv("cardioto_noClass_corr.csv",row.names = 1)
+######## Cardiotocography ########
+  #---------- Data import ----------
+#http://archive.ics.uci.edu/ml/datasets/Cardiotocography
 
 download.file('https://staff.elka.pw.edu.pl/~rbembeni/dane/cardioto_all_corr.csv','cardioto_all_corr.csv')
 ctg_all <- read.csv("cardioto_all_corr.csv",row.names = 1)
 
+download.file('https://staff.elka.pw.edu.pl/~rbembeni/dane/cardioto_noClass_corr.csv','cardioto_noClass_corr.csv')
+ctg_noClass <- read.csv("cardioto_noClass_corr.csv",row.names = 1)
 
+ctg_noClass.kmeans_basic <- eclust(ctg_noClass, "kmeans", k=max(ctg_all$CLASS), graph=TRUE)
 
-#---------- DATA PREPROCESSING ----------
+  #---------- DATA PREPROCESSING ----------
 colnames(ctg_noClass)
 nrow(ctg_noClass)
 colnames(ctg_all)
@@ -33,22 +38,19 @@ sum(duplicated(ctg_noClass))
 data <- data[!duplicated(ctg_noClass), ]
 sum(duplicated(ctg_all))
 data <- data[!duplicated(ctg_all), ]
-
-
-
 # We now have a clean data set to work with
 
-#---------- Data statistics and analysis ----------
+  #---------- Data statistics and analysis ----------
 
-# 1. For ctg_noClass
-# Summary of the columns
-summary(ctg_noClass)
-# Structure of the colmuns and their data types
-str(ctg_noClass)
-# Histograms for numeric columns
-hist(ctg_noClass$Mean, main = "Histogram of Mean", xlab = "Mean")
-# Boxplots to detect potential outliers
-boxplot(ctg_noClass$Variance, main = "Boxplot of Variance", horizontal = TRUE)
+# # 1. For ctg_noClass
+# # Summary of the columns
+# summary(ctg_noClass)
+# # Structure of the colmuns and their data types
+# str(ctg_noClass)
+# # Histograms for numeric columns
+# hist(ctg_noClass$Mean, main = "Histogram of Mean", xlab = "Mean")
+# # Boxplots to detect potential outliers
+# boxplot(ctg_noClass$Variance, main = "Boxplot of Variance", horizontal = TRUE)
 
 #2. For ctg_all
 # Summary of the columns
@@ -64,18 +66,95 @@ table(ctg_all$CLASS)
 # Proportion of each category
 prop.table(table(ctg_all$NSP))
 
-#---------- Clustering ----------
-#example
-distC = dist(ctg_noClass)
-card.kmeans = kmeans(distC,10)
-res3 = table(ctg_all$CLASS,card.kmeans$cluster )
-res3
+  #---------- Clustering ----------
+MAX_NUM_GROUPS <- 15
+
+    #---------- Kmeans ----------
+kmeans.wss <- vector(mode = "integer" ,length = MAX_NUM_GROUPS)
+kmeans.rand_index <- vector(mode = "double" ,length = MAX_NUM_GROUPS)
 
 
-# ------------------------------------------------------------------------------
+for (i in 1:MAX_NUM_GROUPS) {
+  kmean <- eclust(ctg_noClass, "kmeans", k=i, graph=FALSE)
+
+  # Rand index
+  clusters_stats <- cluster.stats(dist(ctg_noClass), ctg_all$CLASS, kmean$cluster)
+  kmeans.rand_index[i] <- clusters_stats$corrected.rand
+  
+  # total within-cluster sum of squares
+  kmeans.wss[i] <- kmean$tot.withinss
+}
+
+# Average silouhette width per number of groups => find optimal number of groups
+fviz_nbclust(ctg_noClass, kmeans, method = "silhouette")+theme_classic()
+
+# total within-cluster sum of squares per number of groups
+plot(1:MAX_NUM_GROUPS, kmeans.wss, type = "b", 
+     xlab = "Number of groups", 
+     ylab = "Total within-cluster sum of squares")
+
+# Rand Index per number of groups
+plot(1:MAX_NUM_GROUPS, kmeans.rand_index, type="b",
+     xlab = "Number of groups",
+     ylab = "Rand index")
+
+    #---------- Partitioning around medoids ----------
+pam.wss <- vector(mode = "integer" ,length = MAX_NUM_GROUPS)
+pam.rand_index <- vector(mode = "double" ,length = MAX_NUM_GROUPS)
 
 
-#wines
+for (i in 1:MAX_NUM_GROUPS) {
+  pam <- eclust(ctg_noClass, "pam", k=i, graph=FALSE)
+  
+  # Rand index
+  clusters_stats <- cluster.stats(dist(ctg_noClass), ctg_all$CLASS, pam$cluster)
+  pam.rand_index[i] <- clusters_stats$corrected.rand
+  
+  # total within-cluster sum of squares
+  pam.wss[i] <- pam$tot.withinss
+}
+
+# Average silouhette width per number of groups
+fviz_nbclust(ctg_noClass, pam, method = "silhouette")+theme_classic()
+
+# total within-cluster sum of squares per number of groups
+plot(1:MAX_NUM_GROUPS, kmeans.wss, type = "b", 
+     xlab = "Number of groups", 
+     ylab = "Total within-cluster sum of squares")
+
+# Rand Index per number of groups
+plot(1:MAX_NUM_GROUPS, kmeans.rand_index, type="b",
+     xlab = "Number of groups",
+     ylab = "Rand index")
+
+    #---------- DBSCAN ----------
+dbscan.eps <- vector(mode = "double" ,length = MAX_NUM_GROUPS)
+dbscan.rand_index <- vector(mode = "double" ,length = MAX_NUM_GROUPS)
+
+
+for (i in 1:MAX_NUM_GROUPS) {
+  dbscan <- dbscan(ctg_noClass, i)
+  
+  # Rand index
+  clusters_stats <- cluster.stats(dist(ctg_noClass), ctg_all$CLASS, dbscan$cluster)
+  dbscan.rand_index[i] <- clusters_stats$corrected.rand
+  
+  dbscan$eps
+}
+
+# total within-cluster sum of squares per number of groups
+plot(1:MAX_NUM_GROUPS, kmeans.wss, type = "b", 
+     xlab = "Number of groups", 
+     ylab = "Total within-cluster sum of squares")
+
+# Rand Index per number of groups
+plot(1:MAX_NUM_GROUPS, dbscan.rand_index, type="b",
+     xlab = "Number of groups",
+     ylab = "Rand index")
+
+  
+######## Wine ########
+  #---------- Data import ----------
 download.file('http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv', 'wine_red.csv');
 download.file('http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv', 'wine_white.csv');
 wineRed_ds = read.table("wine_red.csv", header = TRUE, sep=";", na.strings= "*")
@@ -84,7 +163,7 @@ View(wineRed_ds)
 summary(wineRed_ds)
 
 
-#---------- DATA PREPROCESSING ----------
+  #---------- DATA PREPROCESSING ----------
 colnames(wineRed_ds)
 nrow(wineRed_ds)
 
@@ -98,7 +177,7 @@ data <- data[!duplicated(wineRed_ds), ]
 
 # We now have a clean data set to work with
 
-#---------- Data statistics and analysis ----------
+  #---------- Data statistics and analysis ----------
 
 # Summary of the columns
 summary(wineRed_ds)
@@ -120,7 +199,7 @@ plot(wineRed_ds$alcohol, wineRed_ds$quality,
      main = "Alcohol vs Quality", xlab = "Alcohol", ylab = "Quality")
 
 
-#---------- Clustering ----------
+  #---------- Clustering ----------
 #example - wines
 distC = dist(wineRed_dsC)
 card.kmeans = kmeans(distC,6)
